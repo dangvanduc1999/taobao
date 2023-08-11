cad06866abe44cd2a68ad6f49188b92ae6e7abc9
Van duc <vanduclipi@gmail.com>
2023-08-11 22:12:50 +0700

    [update] Update home

---
+++ js/home.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: false,
+    slidesPerView: 3,
+    spaceBetween: 20,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+});
