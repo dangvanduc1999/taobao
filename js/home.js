61820b8f711fbc4867f9c7d59f29e4070ceda908
Van duc <vanduclipi@gmail.com>
2023-08-16 23:09:26 +0700

    [Feature] Update responsive home

--- js/home.js
+++ js/home.js
@@ -8,4 +8,34 @@ const swiper = new Swiper(".swiper", {
         el: '.swiper-pagination',
         clickable: true
     },
+    breakpoints: {
+        "@0.00": {
+            slidesPerView: 1,
+        },
+        "@0.70": {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        "@0.75": {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        "@0.85": {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        "@1.00": {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        "@1.20": {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+ 
+        "@1.50": {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+    },
 });
