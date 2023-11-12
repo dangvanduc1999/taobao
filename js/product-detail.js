709d77ac54c27f40f28ab6d76ee15cb5ea72c86f
ducdang <vanduclipi@gmail.com>
2023-11-12 18:51:33 +0700

    [fix] fix css and update wishlist

--- js/product-detail.js
+++ js/product-detail.js
@@ -359,6 +359,18 @@ window.onload = function () {
 		},3000)
 	} )
 	
+	const wishLists = document.querySelectorAll("#heart-icon")
+	wishLists.forEach(item => {
+		
+		item.addEventListener("mouseenter", function () {
+			item.classList.remove("ri-heart-3-line")
+			item.classList.add("ri-heart-3-fill")
+		})
+		item.addEventListener("mouseout", function () {
+			item.classList.remove("ri-heart-3-fill")
+			item.classList.add("ri-heart-3-line")
+		})
+	})
 	
 	
 	
